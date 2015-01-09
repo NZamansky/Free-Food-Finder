95ec1978df1161c367c5c444d91d37c13c3c843d
Natan Zamansky <natan.zamansky@gmail.com>
2015-01-09 12:09:41 -0500

    Maybe finally got the map working.

--- static/script.js
+++ static/script.js
@@ -1,8 +1,12 @@
 var mapUrl = "https://www.google.com/maps/embed/v1/view?key=AIzaSyATf-MrlFDWnyXHfZEXgRrZny6xA3r19h";
 
+var latitude;
+var longitude;
+
 function success(pos){
     var coords=pos.coords;
-    console.log(coords.latitude);
+    latitude = coords.latitude;
+    longitude = coords.longitude;
     return coords;
 }
 function failure() {
@@ -14,6 +18,6 @@ geoPosition.init();
 var coords = (geoPosition.getCurrentPosition(success,failure));
 console.log(coords.latitude);
 
-//mapUrl = mapUrl + coords.latitude+','+coords.longitude;
-//var map = document.getElementById('map');
-//map.innerHTML = '<iframe width="400" height="400" src="'+mapUrl+'"></iframe>';
+mapUrl = mapUrl + '&center=' + latitude+','+longitude+"&zoom=13&size=400x400";
+var map = document.getElementById('map');
+map.innerHTML = '<iframe width="400" height="400" src="'+mapUrl+'"></iframe>';
