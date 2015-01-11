fd3dbc30f52edfaee58f545a3f4ef85933df1735
Natan Zamansky <natan.zamansky@gmail.com>
2015-01-11 10:54:47 -0500

    This map thing finally works after dozens of stupid errors.

--- static/script.js
+++ static/script.js
@@ -1,29 +1,21 @@
-var mapUrl = "https://www.google.com/maps/embed/v1/view?key=AIzaSyB0fUiZ7AkUxevDyCRROQd4XaWPwDhrSyE";
 
-var latitude;
-var longitude;
 
-console.log(latitude);
-
-function success(pos){
+var success = function(pos){
     var coords=pos.coords;
-    var latitude = coords.latitude;
-    console.log(latitude);
-    var longitude = coords.longitude;
-    
-    return ""+latitude+","+longitude;
+    var mapUrl = "https://www.google.com/maps/embed/v1/view?key=AIzaSyB0fUiZ7AkUxevDyCRROQd4XaWPwDhrSyE";
+    var lat = coords.latitude;
+    var lng = coords.longitude;
+    mapUrl = mapUrl + '&center='+lat+','+lng+"&zoom=16";
+    var map = document.getElementById('map');
+    map.innerHTML = '<iframe width="400" height="400" src="'+mapUrl+'"></iframe>';
+
 }
-function failure() {
-  alert('ERROR: Position indeterminable');
+var failure = function() {
+    alert('ERROR: Position indeterminable');
 };
 
 geoPosition.init();
 
-var coords = geoPosition.getCurrentPosition(success,failure);
+geoPosition.getCurrentPosition(success,failure);
 
-mapUrl = mapUrl + '&center=' + coords;
 
-console.log(mapUrl);
-
-var map = document.getElementById('map');
-map.innerHTML = '<iframe width="400" height="400" src="'+mapUrl+'"></iframe>';
